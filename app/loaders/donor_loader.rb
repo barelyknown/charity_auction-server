b4511ecf62ad49bb88378ef60b3d07c0e04cb41c
Sean Devine <barelyknown@icloud.com>
2015-11-06 17:08:55 -0500

    remove donor loader class

--- app/loaders/donor_loader.rb
+++
@@ -1,8 +0,0 @@
-class DonorLoader do
-
-  attr_reader :auction, :csv
-
-  def initialize(auction, csv)
-    @auction, @csv = auction, csv
-  end
-end
--- spec/loaders/donor_loader_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe DonorLoader do
-  let! :auction do
-    FactoryGirl.create(:auction)
-  end
-end
